8192f09e94c8c9104ee6f39026f748538baf58d6
조승민 <81403148+whflwhfl@users.noreply.github.com>
2024-11-05 11:26:11 +0900

    Add files via upload

---
+++ code/week-10.sql
@@ -0,0 +1,15 @@
+SELECT * FROM 학생;
+--트랜잭션
+BEGIN;
+--START TRANSACTION;
+DELETE FROM 학생 WHERE 학생.성별 = '남';
+DELETE FROM 학생 WHERE 학생.성별 = '여';
+SELECT * FROM 학생;
+ROLLBACK;
+SELECT * FROM 학생;
+
+BEGIN;
+UPDATE 학생 SET 이름 = '홍길순' WHERE 학번 = 's002'
+SELECT * FROM 학생;
+COMMIT;
+SELECT * FROM 학생;
